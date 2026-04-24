fb19106cc888816b4032d9786304a3dcf7c1126c
PATENTS UNIVERSAL LICENSE <hoanglongvu2024@icloud.com>
2026-04-24 16:09:19 -0500

    Add navigation functions for different pages

---
+++ nav.js
@@ -0,0 +1,7 @@
+function goRoom(){window.location.href="creationsroom.html";}
+function goHub(){window.location.href="project-hub.html";}
+function goAI(){window.location.href="ai-command.html";}
+function goOwnership(){window.location.href="ownership.html";}
+function goUSPTO(){window.location.href="uspto.html";}
+function goCoin(){window.location.href="licensecoin-engine.html";}
+function goGlobal(){window.location.href="globalroom.html";}
